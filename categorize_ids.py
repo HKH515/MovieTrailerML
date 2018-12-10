613b204c87c76b00f3536078fbf61baafc68636f
siggihelga <sigurdurhel15@ru.is>
2018-12-10 20:59:12 +0000

    updated categorize script

--- categorize_ids.py
+++ categorize_ids.py
@@ -3,20 +3,16 @@ import json
 
 frame_info = {
         'good': [],
-        'zero': [],
-        'long': [] # 10 mins is (framescount*100) / 30 > 600 circa
 }
 
-for i,movieid in enumerate(os.listdir("frames")):
+for i,movieid in enumerate(sorted(os.listdir("frames"))):
     amount = len([n for n in os.listdir('frames/{}'.format(movieid)) if os.path.isfile('frames/{}/{}'.format(movieid, n))])
     if i % 100 == 0:
         print("processing {} fn {}".format(movieid, i))
-    if amount == 0:
-        frame_info['zero'].append(movieid)
-    elif (amount*100)/30 > 200 or (amount*100)/30 < 40:
-        frame_info['long'].append(movieid)
+    if (amount*100)/30 > 200 or (amount*100)/30 < 40:
+        pass
     else:
-        frame_info['good'].append(movieid)
+        frame_info['good'].append((movieid,amount))
 
 #frame_json = json.dumps(frame_info)
 
